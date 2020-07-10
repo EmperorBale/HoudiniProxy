4f61e7a9f92a0417b198d0442dddac2e603232dc
Zaseth <hahano@xd.com>
2020-07-10 15:07:05 +0200

    Added isXML and isXT

--- src/proxy/system/handler.js
+++ src/proxy/system/handler.js
@@ -7,6 +7,26 @@
 module.exports = class Handler {
   /**
    * @static
+   * Returns whether the data is XML or not
+   * @param {String} data
+   * @returns {Boolean}
+   */
+  static isXML(data) {
+    return data.charAt() === '<' && data.charAt(data.length - 1) === '>'
+  }
+
+  /**
+   * @static
+   * Returns whether the data is XT or not
+   * @param {String} data
+   * @returns {Boolean}
+   */
+  static isXT(data) {
+    return data.charAt() === '%' && data.charAt(data.length - 1) === '%'
+  }
+
+  /**
+   * @static
    * Handles outgoing data from the proxy to the client
    * @param {String} data
    * @param {Net.Socket} client
